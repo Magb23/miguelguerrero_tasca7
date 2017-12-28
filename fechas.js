4597079e023b48c42d2f49235bfb2225f3a715eb
Magb23 <m.a.guerrero1989@gmail.com>
2017-12-28 11:02:58 +0100

    cambios 28/12

--- fechas.js
+++ fechas.js
@@ -1,5 +1,5 @@
 function calcularsiglo(anyo){
-
+    
     if (anyo>=1700 && anyo<=1799) {
         return 5;
     }
@@ -18,48 +18,50 @@ function calcularsiglo(anyo){
     else if (anyo>=2200 && anyo<=2299) {
         return -4;
     }
-    
+        
 }
 
 function calcularanyo(anyo){
-
-   // var b = Math.trunc(parseInt(b.toString().substr(-2))/4);
-    //return b;
+    
+    /* var b = Math.trunc(parseInt(b.toString().substr(-2))/4);
+    return b;*/
     var f = parseInt(anyo.toString().substr(-2));
     var z = f/4;
     var w = Math.trunc(z);
     var g = f+w;
     return g;
-
-     
+    
 }
 
 function calcularanyobisiesto(anyo){
     
-    if(anyo.toString().substr(-1)=="2" || anyo.toString().substr(-1)== "4" && mes =="enero" || mes == "febrero"){
-        return -1;
+    if((anyo%4==0 || anyo%100!==0) && anyo%400==0){
+        if(mes=="enero" || mes=="febrero"){
+            return -1;
+        }      
     }
     else {
         return 0;
-    }
-    
-
-}
+    }  
+} /*Para la función del año bisiesto he intentado hacer una única línea de código para el IF,
+ pero por algún motivo me detectaba todos los meses como bisiestos y me aplicaba -1, por lo que al ejecutar
+ el algoritmo final "calculardia" me devolvía un día menos de lo que tocaba.
+ Esto ha sido subsanado con otro IF dentro del primero para los meses de enero y febrero*/
 
 function calcularmes(mes){
     
     if (mes=="enero") {
         return 6;
     }
-
+    
     if (mes=="febrero") {
         return 2;
     }
-
+    
     if (mes=="marzo") {
         return 2;
     }
-
+    
     if (mes=="abril") {
         return 5;
     }
@@ -71,23 +73,23 @@ function calcularmes(mes){
     if (mes=="junio") {
         return 3;
     }
-
+    
     if (mes=="julio") {
         return 5;
     }
-
+    
     if (mes=="agosto") {
         return 1;
     }
-
+    
     if (mes=="septiembre") {
         return 4;
     }
-
+    
     if (mes=="octubre") {
         return 6;
     }
-
+    
     if (mes=="noviembre") {
         return 2;
     }
@@ -95,32 +97,32 @@ function calcularmes(mes){
     if (mes=="diciembre") {
         return 4;
     }
-
-
+    
 }
 
-//function dia(dia){
-  //  return(dia);//el dia introducido
-//}
+/*function dia(dia){
+return(dia);//el dia introducido
+}*/
 
 function calculardia(anyo,mes,dia){
+    
     var a = calcularsiglo(anyo);
     var b = calcularanyo(anyo);
     var c = calcularanyobisiesto(anyo);
     var d = calcularmes(mes);
     var e = parseInt(dia);
-
-    var y = a + b + c + d + e;//Tomamos todos los coeficientes calculados y los sumamos
-    var r = y % 7;//después calculamos el resto módulo 7 del número que obtenemos (restamos 7 a y)
-
-
-    console.log(a);
+    
+    var y = a + b + c + d + e; //Tomamos todos los coeficientes calculados y los sumamos
+    var r = y % 7; /*Después calculamos el resto módulo 7 del número que obtenemos (% 7 a la variable "y"),
+    con lo cual obtendremos la variable "r", que es el resultado final que utilizaremos para obtener el día */
+    
+    /*console.log(a);
     console.log(b);
     console.log(c);
     console.log(d);
     console.log(e);
-
-   
+    He utilizado estos "console.log" para poder ver en qué parte del código me fallaba la ejecución*/ 
+    
     if (r==1){
         return "lunes";
     }
